31b3779cce67488d114fb5da0a35590d3492d418
Diego Sepulveda <diegosl@hotmail.cl>
2021-05-17 17:11:09 -0400

    more logic added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,25 +13,38 @@ export const getUserById = async (req: Request, res: Response): Promise<Response
   if (user) {
     return res.status(200).json(user);
   }
-  return res.status(200).json({ msg: "User not found" });
+  return res.status(404).json({ msg: `User id ${id} not found` });
 };
 
 export const createNewUser = async (req: Request, res: Response): Promise<Response> => {
-  const newUser = getRepository(User).create(req.body);
-  const results = await getRepository(User).save(newUser);
-  return res.status(201).json(results);
+  const newUser = req.body;
+  if (!newUser.firstname) {
+    return res.status(400).json({ msg: `You must provide a firstname field` });
+  }
+  if (!newUser.lastname) {
+    return res.status(400).json({ msg: `You must provide a lastname field` });
+  }
+  const result = await getRepository(User).save(newUser);
+  return res.status(201).json(result);
 };
 
 export const updateUserById = async (req: Request, res: Response): Promise<Response> => {
   const { id } = req.params;
-  const user = await getRepository(User).findOne(id);
-  if (user) {
-    user.firstname = req.body.firstname;
-    user.lastname = req.body.lastname;
-    const result = await getRepository(User).save(user);
+  const updatedUser = req.body;
+  if (!updatedUser.firstname) {
+    return res.status(400).json({ msg: `You must provide a firstname field` });
+  }
+  if (!updatedUser.lastname) {
+    return res.status(400).json({ msg: `You must provide a lastname field` });
+  }
+  const userToUpdate = await getRepository(User).findOne(id);
+  if (userToUpdate) {
+    userToUpdate.firstname = updatedUser.firstname;
+    userToUpdate.lastname = updatedUser.lastname;
+    const result = await getRepository(User).save(userToUpdate);
     return res.status(200).json(result);
   }
-  return res.status(404).json({ msg: "User not found" });
+  return res.status(404).json({ msg: `User id ${id} not found` });
 };
 
 export const deleteUserById = async (req: Request, res: Response): Promise<Response> => {
@@ -41,5 +54,5 @@ export const deleteUserById = async (req: Request, res: Response): Promise<Respo
     const result = await getRepository(User).remove(user);
     return res.status(200).json(result);
   }
-  return res.status(404).json({ msg: "User not found" });
+  return res.status(404).json({ msg: `User id ${id} not found` });
 };
